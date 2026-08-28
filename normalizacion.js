fe11e0d338c2eef42e6043e4a175f6c3561d5825
alfredoTous <adtous@uninorte.edu.co>
2026-08-28 14:22:17 -0500

    feat: byppas rate limiting by fetching in chunks and sleeps + initial parse response to json into arrays

--- normalizacion.js
+++ normalizacion.js
@@ -1,4 +1,9 @@
 
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+
 async function normalizar() {
     let main_url = "https://rickandmortyapi.com/api/character/";
     try {
@@ -9,19 +14,24 @@ async function normalizar() {
         let data = await response.json();
         const pages = data.info.pages; // Obtenemos la cantidad de paginas
 
-        const urls = [];
+        let urls = [];
         for (let i=1; i<pages+1; i++) {
             urls.push(`${main_url}?page=${i}`);
         }
-
-        const promesas = urls.map(url => fetch(url));
-        const responses = await Promise.all(promesas);
-
-        data = await Promise.all(
-            responses.map(r => r.json())
-        );
         
-        console.log(data);
+        urls = urls.slice(0,15); // Cortar temporalmente para optimizar pruebas
+        console.log(`[+] Main Url: ${main_url}`);
+        console.log("[+] Parseando de 5 en 5 para evitar rate limiting");
+        console.log("[+] Empezando parsing...")
+        let responses = [];
+        for (let i=0; i<urls.length; i+=5) {
+            let tempResponses = await Promise.all(urls.slice(i, i+5).map(url => fetch(url).then(r => r.json()))); // Array con 5 respuestas, volvemos a json enseguida
+            responses.push(...tempResponses); // Los 3 puntos desempaqueta el array y queda aplanado
+            console.log(`[i] Parseado: ${i+5}/${urls.length}`)
+            await sleep(500);
+        }
+
+        console.log(responses[0].results[0].name); // Rick Sanchez
 
     } catch (error) {
         console.error(`Fetch fallo: ${error}`);
