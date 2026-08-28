b69e4420a16edf09b29d9439d00f2749ec4a7987
alfredoTous <adtous@uninorte.edu.co>
2026-08-28 14:48:55 -0500

    feat: finish parsing of characters into normalized objects

--- normalizacion.js
+++ normalizacion.js
@@ -3,24 +3,19 @@ function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-
 async function normalizar() {
-    let main_url = "https://rickandmortyapi.com/api/character/";
+    let mainUrl = "https://rickandmortyapi.com/api/character/";
     try {
-        const response = await fetch(main_url);
-        if (!response.ok) {
-            console.error(`[-] Request a ${main_url} fallo`);
-        }
-        let data = await response.json();
-        const pages = data.info.pages; // Obtenemos la cantidad de paginas
+        const response = await fetch(mainUrl);
+        let   data     = await response.json();
+        const pages    = data.info.pages; // Obtenemos la cantidad de paginas
 
         let urls = [];
         for (let i=1; i<pages+1; i++) {
-            urls.push(`${main_url}?page=${i}`);
+            urls.push(`${mainUrl}?page=${i}`);
         }
-        
-        urls = urls.slice(0,15); // Cortar temporalmente para optimizar pruebas
-        console.log(`[+] Main Url: ${main_url}`);
+
+        console.log(`[+] Main Url: ${mainUrl}`);
         console.log("[+] Parseando de 5 en 5 para evitar rate limiting");
         console.log("[+] Empezando parsing...")
         let responses = [];
@@ -28,11 +23,29 @@ async function normalizar() {
             let tempResponses = await Promise.all(urls.slice(i, i+5).map(url => fetch(url).then(r => r.json()))); // Array con 5 respuestas, volvemos a json enseguida
             responses.push(...tempResponses); // Los 3 puntos desempaqueta el array y queda aplanado
             console.log(`[i] Parseado: ${i+5}/${urls.length}`)
-            await sleep(500);
+            await sleep(1000);
         }
-
-        console.log(responses[0].results[0].name); // Rick Sanchez
-
+        
+        let characters = [];
+        // Responses son las paginas (42), results es el array de objetos por pagina
+        for (let r of responses) {
+            for (let objData of r.results) {
+                const character = {
+                    id                : objData.id,
+                    nombre            : objData.name,
+                    estado            : objData.status,
+                    especie           : objData.species,
+                    tipo              : objData.type,
+                    genero            : objData.gender,
+                    origen            : objData.origin?.name,
+                    ubicacionActual   : objData.location?.name,
+                    cantidadEpisodios : objData.episode.length,
+                    imagen            : objData.image
+                }
+                characters.push(character);
+            }
+        }
+        console.log(characters);
     } catch (error) {
         console.error(`Fetch fallo: ${error}`);
     }
