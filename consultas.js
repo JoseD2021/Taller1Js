5aad7bd7c079e2b18550ca033b674a165bfae4eb
DevJeffreyG <gabrielpaez.r@hotmail.com>
2026-08-29 20:06:44 -0500

    feat: implementacion 5-6

--- consultas.js
+++ consultas.js
@@ -1,27 +1,34 @@
 async function findAllCharacters() {
+    let response = [];
     try {
         let res = await fetch(`https://rickandmortyapi.com/api/character`);
         let allChars = await res.json();
 
         let { pages } = allChars.info;
 
-        let response = allChars.results;
+        response = response.concat(allChars.results);
 
         for (let i = 2; i <= pages; i++) {
-            console.log("Obteniendo información de la página " + i);
-            fetch(`https://rickandmortyapi.com/api/character?page=${i}`)
-                .then(res => res.json())
-                .then(res => response.concat(res.results))
-                .catch(e => console.error("No se pudo obtener la información de la página " + i, e));
-        }
+            // console.log("Obteniendo información de la página " + i);
 
-        return response;
+            let pageRes = await fetch(`https://rickandmortyapi.com/api/character?page=${i}`);
+
+            if (pageRes.status !== 200) {
+                console.error("No se pudo obtener la información de la página " + i);
+                console.error("Status: " + pageRes.status);
+                console.error(pageRes.statusText);
+                continue;
+            }
+
+            let pageData = await pageRes.json();
+
+            response = response.concat(pageData.results);
+        }
     } catch (e) {
         console.error("No se pudo obtener la información de los personajes", e);
-
-        return [];
     }
 
+    return response;
 }
 
 function findAlive(allChars = []) {
@@ -47,20 +54,64 @@ function hasImageAndEpisode(allChars = []) {
     return allChars.every(x => x.image && x.image.length > 0 && x.episode.length > 0);
 }
 
+function groupCharacters(allChars = []) {
+    let reduced = allChars.reduce((acc, actual) => {
+        acc[actual.species] = acc[actual.species] || { cantidad: 0, totalEpisodios: 0, promedioEpisodios: 0, vivos: 0 };
+
+        let accActual = acc[actual.species];
+
+        accActual.cantidad += 1;
+        accActual.totalEpisodios += actual.episode.length;
+        accActual.promedioEpisodios = accActual.totalEpisodios / accActual.cantidad;
+        accActual.vivos += actual.status.toUpperCase() == "ALIVE" ? 1 : 0;
+
+        return acc;
+    }, {});
+
+    // eliminar totalEpisodios de cada especie
+    for (let especie in reduced) {
+        delete reduced[especie].totalEpisodios;
+    }
+
+    return reduced;
+}
+
+function groupByEpisodes(allChars = []) {
+    return allChars.reduce((acc, actual) => {
+        let episodes = actual.episode.length;
+
+        if (episodes >= 1 && episodes <= 5) {
+            acc["1-5"] += 1;
+        } else if (episodes >= 6 && episodes <= 15) {
+            acc["6-15"] += 1;
+        } else if (episodes >= 16 && episodes <= 30) {
+            acc["16-30"] += 1;
+        } else if (episodes > 30) {
+            acc["30+"] += 1;
+        }
+
+        return acc;
+    }, { "1-5": 0, "6-15": 0, "16-30": 0, "30+": 0 })
+}
+
 let allChars = findAllCharacters()
     .then(allChars => {
+        console.log("------------------------------------------------")
         console.log("Se encontraron " + allChars.length + " personajes en total");
         let alive = findAlive(allChars);
         let moreThan20Episodes = moreThan20(allChars);
         let alienFemaleChar = alienFemale(allChars);
         let someTypeChar = someType(allChars);
         let hasImageAndEpisodeChar = hasImageAndEpisode(allChars);
+        let groupedCharacters = groupCharacters(allChars);
+        let groupedByEpisodes = groupByEpisodes(allChars);
 
-        console.log("TODOS LOS PERSONAJES VIVOS:")
+        console.log("------------------------------------------------")
+        console.log(`PERSONAJES VIVOS (${alive.length})`)
         console.log(alive)
 
         console.log("------------------------------------------------")
-        console.log("TODOS LOS PERSONAJES QUE APARECEN EN 20 O MÁS EPISODIOS:")
+        console.log(`PERSONAJES QUE APARECEN EN 20 O MÁS EPISODIOS (${moreThan20Episodes.length})`)
         console.log(moreThan20Episodes)
 
         console.log("------------------------------------------------")
@@ -74,5 +125,13 @@ let allChars = findAllCharacters()
         console.log("------------------------------------------------")
         console.log("Todos los personajes tienen imagen y aparecen en algun episodio?:")
         console.log(hasImageAndEpisodeChar)
+
+        console.log("------------------------------------------------")
+        console.log("AGRUPACIÓN DE PERSONAJES POR ESPECIE:")
+        console.log(groupedCharacters)
+
+        console.log("------------------------------------------------")
+        console.log("AGRUPACIÓN DE PERSONAJES POR CANTIDAD DE EPISODIOS:")
+        console.log(groupedByEpisodes)
     })
     .catch(e => console.error("No se pudo obtener la información de los personajes", e));
